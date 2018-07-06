6a811bb3fa73cab152cef6a57f49ba7a0b7b65bf
Paul L <plennon@ait.ie>
2018-07-06 15:05:08 +0100

    First Git Commit.

---
+++ src/main/java/com/example/one/Example1.java
@@ -0,0 +1,21 @@
+package com.example.one;
+
+public class Example1 {
+	
+	public Example1()
+	{
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a test");
+
+	}
+	public void add(int first, int second)
+	{
+		int total = first + second;
+		
+	}
+
+}
---
+++ src/test/java/com/example/test/TestExample1.java
@@ -0,0 +1,28 @@
+package com.example.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.example.one.Example1;
+
+public class TestExample1 {
+
+	Example1 testing;
+	@Before
+	public void setUp() throws Exception {
+		testing = new Example1();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+}
